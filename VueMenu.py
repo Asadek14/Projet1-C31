c3b86d348a3e476519c89dd72ba95c71dd771510
figrmarcelo <frmarcelocontact@gmail.com>
2022-09-15 17:12:54 -0400

    Affiche le menu

---
+++ VueMenu.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+class VueMenu:
+    root = Tk()
+    root.geometry('800x400')
+
+    Label(root,
+              text="Jeu des Daleks",
+  
+              font=("Arial", 13)
+              ).pack()
+
+    root.mainloop()
